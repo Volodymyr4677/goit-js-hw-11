87a67a024ec4784b6344627c80178c406a2907f4
Volodymyr Prudius <vova46772020@gmail.com>
2024-11-04 20:45:05 +0100

    помилка izitoast в консолі

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,6 +1,5 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -8,6 +7,7 @@ export const renderImages = images => {
   const gallery = document.querySelector('.gallery');
 
   gallery.innerHTML = '';
+
   if (images.length === 0) {
     iziToast.error({
       title: 'Error',
@@ -21,26 +21,26 @@ export const renderImages = images => {
   const markup = images
     .map(
       item => `
-            <div class="photo-card">
-                <a href="${item.largeImageURL}">
-                    <img src="${item.webformatURL}" alt="${item.tags}" />
-                </a>
-                <div class="info">
-                    <i class="info-item">
-                        <b>Likes</b> ${item.likes}
-                    </i>
-                    <p class="info-item">
-                        <b>Views</b> ${item.views}
-                    </p>
-                    <p class="info-item">
-                        <b>Comments</b> ${item.comments}
-                    </p>
-                    <p class="info-item">
-                        <b>Downloads</b> ${item.downloads}
-                    </p>
-                </div>
-            </div>
-        `
+        <div class="photo-card">
+          <a href="${item.largeImageURL}">
+            <img src="${item.webformatURL}" alt="${item.tags}" />
+          </a>
+          <div class="info">
+            <i class="info-item">
+              <b>Likes</b> ${item.likes}
+            </i>
+            <p class="info-item">
+              <b>Views</b> ${item.views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b> ${item.comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b> ${item.downloads}
+            </p>
+          </div>
+        </div>
+      `
     )
     .join('');
 
